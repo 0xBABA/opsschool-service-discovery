0c5daee07c58cd0ca91153a193792050bf7b2c22
yoad <yoadlanger@gmail.com>
2021-12-12 22:19:05 +0200

    defined seperate security groups for consul server and agent to allow http traffic to agent running webserver service

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -1,5 +1,50 @@
-resource "aws_security_group" "consul-sg" {
-  name        = "consul-sg"
+resource "aws_security_group" "consul-agent-sg" {
+  name        = "consul-sg-agent"
+  description = "Allow ssh, consul and tcp inbound traffic"
+  vpc_id      = aws_vpc.consul_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    self        = true
+    description = "Allow all inside security group"
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow ssh from the world"
+  }
+
+  ingress {
+    from_port   = 8500
+    to_port     = 8500
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow consul UI access from the world"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outside security group"
+  }
+}
+
+resource "aws_security_group" "consul-server-sg" {
+  name        = "consul-sg-server"
   description = "Allow ssh & consul inbound traffic"
   vpc_id      = aws_vpc.consul_vpc.id
 
